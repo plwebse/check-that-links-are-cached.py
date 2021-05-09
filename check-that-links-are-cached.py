c0c77ee165d3d7f1eb3c888b9c74277955aca77b
plwebse <peter.lindblom@plweb.se>
2021-05-09 21:42:07 +0200

    Moved code to classes

--- check-that-links-are-cached.py
+++ check-that-links-are-cached.py
@@ -4,86 +4,110 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
-def parse_all_a_tags_href_attr_values_from(html):
-    hrefs = set()
-    soup = BeautifulSoup(html, 'html.parser')
-    for link in soup.find_all('a'):
-        hrefs.add(link.get('href'))
-    return hrefs
+class ParseHtmlForAllUrlsInATagsHrefAttributes:
+    def __init__(self, html):
+        self.urls = self.__remove_url_that_dont_start_with_http(
+            self.__parse_all_a_tags_href_attr_values_from(html))
+
+    def __parse_all_a_tags_href_attr_values_from(self, html):
+        hrefs = set()
+        soup = BeautifulSoup(html, 'html.parser')
+        for link in soup.find_all('a'):
+            hrefs.add(link.get('href'))
+        return hrefs
+
+    def __remove_url_that_dont_start_with_http(self, hrefs):
+        urls = []
+        for href in hrefs:
+            if href.startswith('http'):
+                urls.append(href)
+        return urls
+
+    def getParsedUrls(self):
+        return self.urls
 
 
-def remove_url_that_dont_start_with_http(hrefs):
-    urls = []
-    for href in hrefs:
-        if href.startswith('http'):
-            urls.append(href)
-    return urls
+class HttpUtil:
+
+    @staticmethod
+    def get_headers_for_url(url, http_headers):
+        output = url
+        http_response = HttpUtil.__open_url(url)
+        if http_response is None:
+            return output + "\t0"
+        output += "\t" + str(http_response.status)
+        for http_header in http_headers:
+            header = http_response.headers[http_header]
+            if header is not None:
+                output += "\t" + header
+        return output
+
+    @staticmethod
+    def get_html_from(url):
+        http_response = HttpUtil.__open_url(url)
+        if http_response is None:
+            return ""
+        return http_response.read()
+
+    @staticmethod
+    def __open_url(url):
+        try:
+            http_response = urllib.request.urlopen(url)
+            return http_response
+        except urllib.error.HTTPError as e:
+            # "e" can be treated as a http.client.HTTPResponse object
+            return e
+        except Exception as e:
+            return None
 
 
-def get_headers_for_url(url, print_http_headers):
-    output = url
-    http_response = open_url(url)
-    if http_response is None:
-        return output + "\t0"
-    output += "\t" + str(http_response.status)
-    for print_http_header in print_http_headers:
-        header = http_response.headers[print_http_header]
-        if header is not None:
-            output += "\t" + header
-    return output
+class ParseValuesOrReturnDefaultValues:
+    def __init__(self, defaultHttpHeaders, defaultNrOfTimes):
+        self.url = None
+        self.httpHeaders = defaultHttpHeaders
+        self.nrOfTimes = range(defaultNrOfTimes)
 
+        argv = sys.argv[1:]
 
-def get_html_from(url):
-    http_response = open_url(url)
-    if http_response is None:
-        return ""
-    return http_response.read()
+        try:
+            opts, _args = getopt.getopt(argv, "u:t:h:")
+        except:
+            print("An error occured")
+        for opt, arg in opts:
+            if opt in ['-u']:
+                self.url = arg
+            elif opt in ['-t']:
+                self.nrOfTimes = range(int(arg))
+            elif opt in ['-h']:
+                self.httpHeaders = arg.split(',')
 
+    def getUrl(self):
+        return self.url
 
-def open_url(url):
-    try:
-        http_response = urllib.request.urlopen(url)
-        return http_response
-    except urllib.error.HTTPError as e:
-        # "e" can be treated as a http.client.HTTPResponse object
-        return e
-    except Exception as e:
-        return None
+    def gethttpHeaders(self):
+        return self.httpHeaders
+
+    def getNrOfTimes(self):
+        return self.nrOfTimes
 
 
 def main():
-    argv = sys.argv[1:]
-    url = None
 
-    # default values
-    times = range(1)
-    list_http_headers = ["cache-control", "via", "x-cache"]
+    values = ParseValuesOrReturnDefaultValues(
+        ["cache-control", "via", "x-cache"], 1)
 
-    try:
-        opts, _args = getopt.getopt(argv, "u:t:h:")
-    except:
-        print("An error occured")
-    for opt, arg in opts:
-        if opt in ['-u']:
-            url = arg
-        elif opt in ['-t']:
-            times = range(int(arg))
-        elif opt in ['-h']:
-            list_http_headers = arg.split(',')
-
-    if(url is None):
+    if(values.getUrl() is None):
         print("Usage python " + sys.argv[0] + " -u https://www.plweb.se/")
         return
 
-    print("Checking headers " + str(list_http_headers) +
-          " for all urls in html @ url:" + url + " " + str(len(times)) + " times")
+    print("Checking headers " + str(values.gethttpHeaders()) +
+          " for all urls in html @ url:" + values.getUrl() + " " + str(len(values.getNrOfTimes())) + " nr of times")
 
-    html = get_html_from(url)
-    hrefs = parse_all_a_tags_href_attr_values_from(html)
-    urls = remove_url_that_dont_start_with_http(hrefs)
+    html = HttpUtil.get_html_from(values.getUrl())
+    urls = ParseHtmlForAllUrlsInATagsHrefAttributes(html).getParsedUrls()
     for url in urls:
-        for _time in times:
-            print(get_headers_for_url(url, list_http_headers))
+        for _time in values.getNrOfTimes():
+            print(HttpUtil.get_headers_for_url(url, values.gethttpHeaders()))
 
 
 if __name__ == "__main__":
